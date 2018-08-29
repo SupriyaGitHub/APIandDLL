b9ecf3aee3a7e23b658b7e8c8e510ec296b18391
SupriyaGitHub <39467863+SupriyaGitHub@users.noreply.github.com>
2018-08-29 16:36:52 +0000

    Update Startup.cs

--- TestAuth/Startup.cs
+++ TestAuth/Startup.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,7 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using TestAPI.Utils;
+using Microsoft.IdentityModel.Tokens;
+//using TestAPI.Utils;
 
 namespace TestAuth
 {
