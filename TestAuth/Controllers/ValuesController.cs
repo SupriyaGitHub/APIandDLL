add24daedc66284e27ccb69afd96dbd93614a6c0
SupriyaGitHub <39467863+SupriyaGitHub@users.noreply.github.com>
2018-08-30 03:34:01 +0530

    Update ValuesController.cs

--- TestAuth/Controllers/ValuesController.cs
+++ TestAuth/Controllers/ValuesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TestAuth.Controllers
 {
     [Route("api/Auth/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ValuesController : Controller
     {
         // GET api/values
