ee23491c05087c61d51051e4ec558aa34e55d63c
SupriyaGitHub <39467863+SupriyaGitHub@users.noreply.github.com>
2018-08-29 17:05:17 +0000

    Add files via upload

---
+++ TestAuth/Controllers/TockenController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TestAuth.Controllers
+{
+    public static class JwtSecurityKey
+    {
+        public static SymmetricSecurityKey Create(string secret)
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+        }
+    }
+
+    public sealed class JwtToken
+    {
+        private JwtSecurityToken token;
+
+        internal JwtToken(JwtSecurityToken token)
+        {
+            this.token = token;
+        }
+
+        public DateTime ValidTo => token.ValidTo;
+        public string Value => new JwtSecurityTokenHandler().WriteToken(this.token);
+    }
+
+    public sealed class JwtTokenBuilder
+    {
+        private SecurityKey securityKey = null;
+        private string subject = "";
+        private string issuer = "";
+        private string audience = "";
+        private Dictionary<string, string> claims = new Dictionary<string, string>();
+        private int expiryInMinutes = 5;
+
+        public JwtTokenBuilder AddSecurityKey(SecurityKey securityKey)
+        {
+            this.securityKey = securityKey;
+            return this;
+        }
+
+        public JwtTokenBuilder AddSubject(string subject)
+        {
+            this.subject = subject;
+            return this;
+        }
+
+        public JwtTokenBuilder AddIssuer(string issuer)
+        {
+            this.issuer = issuer;
+            return this;
+        }
+
+        public JwtTokenBuilder AddAudience(string audience)
+        {
+            this.audience = audience;
+            return this;
+        }
+
+        public JwtTokenBuilder AddClaim(string type, string value)
+        {
+            this.claims.Add(type, value);
+            return this;
+        }
+
+        public JwtTokenBuilder AddClaims(Dictionary<string, string> claims)
+        {
+            this.claims.Union(claims);
+            return this;
+        }
+
+        public JwtTokenBuilder AddExpiry(int expiryInMinutes)
+        {
+            this.expiryInMinutes = expiryInMinutes;
+            return this;
+        }
+
+        public JwtToken Build()
+        {
+            EnsureArguments();
+
+            var claims = new List<Claim>
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, this.subject),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }
+            .Union(this.claims.Select(item => new Claim(item.Key, item.Value)));
+
+            var token = new JwtSecurityToken(
+                              issuer: this.issuer,
+                              audience: this.audience,
+                              claims: claims,
+                              expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                              signingCredentials: new SigningCredentials(
+                                                        this.securityKey,
+                                                        SecurityAlgorithms.HmacSha256));
+
+            return new JwtToken(token);
+        }
+
+        #region " private "
+
+        private void EnsureArguments()
+        {
+            if (this.securityKey == null)
+                throw new ArgumentNullException("Security Key");
+
+            if (string.IsNullOrEmpty(this.subject))
+                throw new ArgumentNullException("Subject");
+
+            if (string.IsNullOrEmpty(this.issuer))
+                throw new ArgumentNullException("Issuer");
+
+            if (string.IsNullOrEmpty(this.audience))
+                throw new ArgumentNullException("Audience");
+        }
+
+        #endregion
+    }
+
+
+    [Produces("application/json")]
+    [Route("api/Auth/Tocken")]
+    public class TockenController : Controller
+    {
+        [HttpGet("{username}/{password}")]
+        public IActionResult GetToken(string username, string password) // token value
+        {
+            if (string.Compare(username, "manjeet", true) == 0 || string.Compare(username, "dipak", true) == 0 || string.Compare(username, "farid", true) == 0)
+            {
+                var token = new JwtTokenBuilder()
+                               .AddSecurityKey(JwtSecurityKey.Create("fusioncdsprivatekey"))
+                               .AddSubject("fusioncds")
+                               .AddIssuer("fusioncds")
+                               .AddAudience("fusioncds")
+                               .AddClaim("fusioncdsclaim", "1")
+                               .AddExpiry(300)
+                               .Build();
+
+                return Ok(token.Value);
+            }
+            return Unauthorized();
+        }
+    }
+}
+
+
+
