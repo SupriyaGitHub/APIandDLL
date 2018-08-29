e12b7d976636001664fb7296e7f41f4759889009
SupriyaGitHub <39467863+SupriyaGitHub@users.noreply.github.com>
2018-08-29 15:19:00 +0530

    TestAuth namespace

--- TestAuth/Controllers/ValuesController.cs
+++ TestAuth/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TestAPI.Controllers
+namespace TestAuth.Controllers
 {
     [Route("api/Auth/[controller]")]
     public class ValuesController : Controller
