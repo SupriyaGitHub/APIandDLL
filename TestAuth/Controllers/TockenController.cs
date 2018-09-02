06f82e39ce6b1e3262461a58fa29895ab4b79792
SupriyaGitHub <39467863+SupriyaGitHub@users.noreply.github.com>
2018-09-03 01:33:19 +0530

    logic updated for token
    logic updated for token

--- TestAuth/Controllers/TockenController.cs
+++ TestAuth/Controllers/TockenController.cs
@@ -1,13 +1,12 @@
-﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace TestAuth.Controllers
 {
@@ -128,27 +127,200 @@ namespace TestAuth.Controllers
 
 
     [Produces("application/json")]
-    [Route("api/Auth/Tocken")]
-    public class TockenController : Controller
+    [Route("api/AuthService/Tocken")]
+    public class TokenController : Controller
     {
-        [HttpGet("{username}/{password}")]
-        public IActionResult GetToken(string username, string password) // token value
+        private static Dictionary<string, JwtToken> dictAuth;
+
+        static TokenController()
+        {
+            dictAuth = new Dictionary<string, JwtToken>();
+        }
+
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            string key = "asdfgfhfghfjdythmgmtumudkdgtjktuktdukudktukuykuyk@#$@$%#^$&#%^*&*^&(^**^(*(*&kuyky";
+            //System.Windows.Forms.MessageBox.Show(key);
+            //If hashing use get hashcode regards to your key
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //Always release the resources and flush data
+                // of the Cryptographic service provide. Best Practice
+
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0,
+              toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public static string Decrypt(string cipherString, bool useHashing)
+        {
+            byte[] keyArray;
+            //get the byte code of the string
+
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+
+            //Get your key from config file to open the lock!
+            string key = "asdfgfhfghfjdythmgmtumudkdgtjktuktdukudktukuykuyk@#$@$%#^$&#%^*&*^&(^**^(*(*&kuyky";
+            if (useHashing)
+            {
+                //if hashing was used get the hash code with regards to your key
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                //release any resource held by the MD5CryptoServiceProvider
+
+                hashmd5.Clear();
+            }
+            else
+            {
+                //if hashing was not implemented get the byte code of the key
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. 
+            //We choose ECB(Electronic code Book)
+
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(
+                                 toEncryptArray, 0, toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor                
+            tdes.Clear();
+            //return the Clear decrypted TEXT
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+
+        private bool Authenticate(string username, string password)
         {
             if (string.Compare(username, "manjeet", true) == 0 || string.Compare(username, "dipak", true) == 0 || string.Compare(username, "farid", true) == 0)
             {
-                var token = new JwtTokenBuilder()
-                               .AddSecurityKey(JwtSecurityKey.Create("fusioncdsprivatekey"))
-                               .AddSubject("fusioncds")
-                               .AddIssuer("fusioncds")
-                               .AddAudience("fusioncds")
-                               .AddClaim("fusioncdsclaim", "1")
-                               .AddExpiry(300)
-                               .Build();
-
-                return Ok(token.Value);
+                return true;
             }
+            return false;
+        }
+
+        private JwtToken GetJwtToken(string username, string password)
+        {
+            var token = new JwtTokenBuilder()
+                              .AddSecurityKey(JwtSecurityKey.Create("fusioncdsprivatekey"))
+                              .AddSubject("fusioncds")
+                              .AddIssuer("fusioncds")
+                              .AddAudience("fusioncds")
+                              .AddClaim("fusioncdsclaim", "1")
+                              .AddExpiry(300)
+                              .Build();
+
+            return token;
+        }
+
+        private static JwtSecurityToken ValidateAndDecode(string jwt, SecurityKey signingKeys)
+        {
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = "fusioncds",
+                ValidAudience = "fusioncds",
+                IssuerSigningKey = signingKeys
+            };
+
+            try
+            {
+                var claimsPrincipal = new JwtSecurityTokenHandler()
+                    .ValidateToken(jwt, validationParameters, out var rawValidatedToken);
+
+                return (JwtSecurityToken)rawValidatedToken;
+
+            }
+            catch (SecurityTokenValidationException stvex)
+            {
+                // The token failed validation!
+                // TODO: Log it or display an error.
+                throw new Exception($"Token failed validation: {stvex.Message}");
+            }
+            catch (ArgumentException argex)
+            {
+                // The token was not well-formed or was invalid for some other reason.
+                // TODO: Log it or display an error.
+                throw new Exception($"Token was invalid: {argex.Message}");
+            }
+        }
+
+
+
+
+        [HttpGet("{username}/{password}")]
+        public IActionResult GetToken(string username, string password) // token value
+        {
+            if (Authenticate(username,password))
+            {
+                var token = GetJwtToken(username, password);
+                var guid = Guid.NewGuid().ToString();
+                var accesstoken = Encrypt(guid, true);
+                dictAuth.Add(guid, token);
+                return Ok(accesstoken);
+            }
+
             return Unauthorized();
         }
+
+
+        [HttpGet("{key}")]        
+        public IActionResult GetValidateToken(string key) // token value
+        {
+            try
+            {
+                var token = key;
+                token = Decrypt(token, true);
+                if (dictAuth.ContainsKey(token))
+                {
+                    JwtToken jwtToken = dictAuth[token];
+
+                    SecurityKey keys = JwtSecurityKey.Create("fusioncdsprivatekey");
+
+                    JwtSecurityToken jwtseqToken = ValidateAndDecode(jwtToken.Value, keys);
+
+                    return Ok(jwtseqToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO log
+            }
+            
+            return Unauthorized();
+        }
+    
     }
 }
 
